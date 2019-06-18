a8ffce62f292d549a0b5d6386ec9eb08d512c440
Lyunho Kim <kimlh2@gmail.com>
2019-06-19 08:30:09 +0900

    init commit

---
+++ FirestorePlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestorePlayground
+//
+//  Created by Lyunho Kim on 19/06/2019.
+//  Copyright © 2019 Lyunho Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
